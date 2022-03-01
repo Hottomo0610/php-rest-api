f8c18eb33b650ae8185fbb692676a446e199a66c
Hottomo0610 <hottomo0610@gmail.com>
2022-03-01 23:07:17 +0900

    update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ $app = new Pop();
 
 // Home page: http://localhost:8000/
 $app->get('/', function() {
-    http_response_code( 400 );
+    http_response_code( 404 );
     echo 'Hello World!';
 });
 
